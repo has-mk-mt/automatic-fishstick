63a014e94f88cce10cc7a63fc0133c405f1cd2e1
has-mk-mt <hsm.mk@outlook.com>
2025-01-01 14:57:14 +0000

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
-    // /**
-    //  * Register any application services.
-    //  */
-    // public function register(): void
-    // {
-    //     //
-    // }
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-    // /**
-    //  * Bootstrap any application services.
-    //  */
-    // public function boot(): void
-    // {
-    //     //
-    // }
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
 }
